cd38adca51e24d8e42e1e6c9e50e75e2c88b421e
Dmitrii Perfilev <70065740+Dmitrii173173@users.noreply.github.com>
2024-09-08 22:37:50 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,8 @@
-import reflex as rx
 import os
 from decouple import config 
 
 DATABASE_URL = config("DATABASE_URL")
-railway_public_domain = "RAILWAY_PUBLIC_DOMAIN"
+RAILWAY_PUBLIC_DOMAIN = os.getenv("RAILWAY_PUBLIC_DOMAIN", "default-domain")
 
 class ReflextemplateConfig(rx.Config):
     pass
@@ -11,9 +10,8 @@ class ReflextemplateConfig(rx.Config):
 config = ReflextemplateConfig(
     app_name="reflex_gpt",
     telemetry_enabled=False,
-    frontend_port=3000, # default frontend port
-    backend_port=8000, # default backend port
-    # Use public domain for API URL if available, otherwise default to local address
-    api_url=f'https://{os.getenv(railway_public_domain, "127.0.0.1:8000")}/backend',
+    frontend_port=3000, 
+    backend_port=8000, 
+    api_url=f'https://{RAILWAY_PUBLIC_DOMAIN}/backend',
     db_url=DATABASE_URL
 )
